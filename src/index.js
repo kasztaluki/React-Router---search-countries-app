f272c45aaf418b77d41f12fc6356cca72b210c45
Łuki Kasztaniewicz <grabowski.lukasz.88@gmail.com>
2018-06-18 22:24:57 +0200

    Country app part 2

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Router, hashHistory } from 'react-router';
 import { Provider } from 'react-redux';
 import store from './store';
-import DevTools from './DevTools';
+import routes from './routes';
 
 render(
     <Provider store={store}>
-        <div>
-            <DevTools/>
-            <h1>Inicjalizacja projektu</h1>
-        </div>
+        <Router history={hashHistory} routes={routes}/>
     </Provider>,
     document.getElementById('root')
 );
